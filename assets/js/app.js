f49f4ecf48fda9adc7decba6f3b26d597bdbeb89
andeyan <andeyan@qwords.co.id>
2022-02-14 20:42:16 +0700

    combine bs3 & bs5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,27 @@
 const showBtn = document.getElementById('show-location')
 const showBtnHandle = document.getElementById('handle')
+const pillsPedia1 = document.getElementById('pills-pediasure-tab')
+const pillsPedia2 = document.getElementById('pills-pediacomplete-tab')
+const pillsPedia1Content = document.getElementById('pills-pediasure')
+const pillsPedia2Content = document.getElementById('pills-pediacomplete')
+
+pillsPedia1.addEventListener('click', () => {
+   pillsPedia1.classList.add('active')
+   pillsPedia2.classList.remove('active')
+   pillsPedia1Content.classList.add('show__')
+   pillsPedia1Content.classList.add('active')
+})
+
+pillsPedia2.addEventListener('click', () => {
+   pillsPedia2.classList.add('active')
+   pillsPedia1.classList.remove('active')
+   pillsPedia2Content.classList.add('show__')
+   pillsPedia2Content.classList.add('active')
+   pillsPedia1Content.classList.remove('show__')
+   pillsPedia1Content.classList.remove('active')
+})
+
+
 
 showBtn.addEventListener('click', () => {
    showHideLocation();
