75608d5104ff63e2639af0aa6ccdb5303a6b1ab8
andeyan <andeyan@qwords.co.id>
2022-02-15 22:26:47 +0700

    responsive layout cariPedia

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 const showBtn = document.getElementById('show-location')
-const showBtnHandle = document.getElementById('handle')
+const showBtnHandle = document.getElementById('handle-location')
 const pillsPedia1 = document.getElementById('pills-pediasure-tab')
 const pillsPedia2 = document.getElementById('pills-pediacomplete-tab')
 const pillsPedia1Content = document.getElementById('pills-pediasure')
 const pillsPedia2Content = document.getElementById('pills-pediacomplete')
+const filterButton = document.getElementById('filter-button')
+const filterHandle = document.getElementById('handle-filter')
 
 pillsPedia1.addEventListener('click', () => {
    pillsPedia1.classList.add('active')
@@ -21,8 +23,6 @@ pillsPedia2.addEventListener('click', () => {
    pillsPedia1Content.classList.remove('active')
 })
 
-
-
 showBtn.addEventListener('click', () => {
    showHideLocation();
    showBtn.classList.add('d-none')
@@ -33,7 +33,21 @@ showBtnHandle.addEventListener('click', () => {
    showBtn.classList.toggle('d-none')
 })
 
+filterButton.addEventListener('click', () => {
+   showFilterNav()
+})
+
+filterHandle.addEventListener('click', () => {
+   showFilterNav()
+})
+
+const showFilterNav = () => {
+   const filterNav = document.getElementById('nav-show-filter')
+   filterNav.classList.toggle('d-none')
+}
+
 const showHideLocation = () => {
 	const locContetn = document.getElementById('location-content')
 	locContetn.classList.toggle('d-none')
 }
+
